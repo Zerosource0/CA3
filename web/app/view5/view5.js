247479448ee80bff2a9433c9d2f6618d8b548887
Stargarth <adamek1221@gmail.com>
2015-11-24 11:24:01 +0100

    Form for creating users

--- web/app/view5/view5.js
+++ web/app/view5/view5.js
@@ -1,7 +1,9 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+'use strict';
 
+angular.module('myApp.view5', ['ngRoute'])
 
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view5', {
+    templateUrl: 'app/view5/view5.html'
+  });
+}]);
