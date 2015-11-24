b7560189d249d203eed892095134c682ae26e500
Stargarth <adamek1221@gmail.com>
2015-11-24 13:54:10 +0100

    Front end add user

--- web/app/view5/view5.js
+++ web/app/view5/view5.js
@@ -4,6 +4,20 @@ angular.module('myApp.view5', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view5', {
-    templateUrl: 'app/view5/view5.html'
+    templateUrl: 'app/view5/view5.html',
+    controller: 'AppNewUserCtrl'
   });
+}])
+
+.controller('AppNewUserCtrl', ['$http','$scope', function($http, $scope)
+{
+	$http.post('api/newuser', $scope.newUser)
+	.success(function(data)
+	{
+		console.log(data);
+	})
+	.error(function(data, status)
+	{
+		console.log("ERROR: "+data, status);
+	});
 }]);
