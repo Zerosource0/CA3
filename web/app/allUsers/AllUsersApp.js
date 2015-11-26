d14acb22b5e6cc0f5ca22a17516a96b139b22b86
Zerosource0 <marcjesse92@hotmail.com>
2015-11-26 14:58:40 +0100

    Revert "Admin page"
    This reverts commit 23d6b9d783c99b9d283669cf6578536608851783.

--- web/app/allUsers/AllUsersApp.js
+++ web/app/allUsers/AllUsersApp.js
@@ -2,39 +2,20 @@
 
 angular.module('myApp.allUsers', ['ngRoute'])
 
-        .config(['$routeProvider', function ($routeProvider) {
-                $routeProvider.when('/allUsers', {
-                    templateUrl: 'app/allUsers/AllUsersView.html',
-                    controller: 'allUsersCtrl',
-                });
-            }])
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/allUsers', {
+    templateUrl: 'app/allUsers/AllUsersView.html',
+    controller: 'allUsersCtrl'
+  });
+}])
 
-        .controller('allUsersCtrl', function ($http, $scope) {
-            $http.get('api/demoadmin')
-                    .success(function (data, status, headers, config) {
-                        $scope.data = data;
-                    })
-                    .error(function (data, status, headers, config) {
-                        console.log(status + " " + headers + " " + data);
-                        
-                    });
-
-            $http.get('api/demoadmin/users')
-                    .success(function (data, status, headers, config) {
-                        $scope.users = data;
-                    })
-                    .error(function (data, status, headers, config) {
-                        console.log(status + " " + headers + " " + data);
-                    });
-
-            $scope.delete = function (id) {
-                $http({
-                    method: 'DELETE',
-                    url: 'api/demoadmin/users/' + id
-                }).then(function successCallback(response) {
-                    console.log(response);
-                }, function errorCallback(response) {
-                    console.log(response);
-                });
-            };
-        });
+.controller('allUsersCtrl', function($http,$scope) {
+  $http.get('api/demoadmin')
+            .success(function (data, status, headers, config) {
+              $scope.data = data;
+            })
+            .error(function (data, status, headers, config) {
+              
+             });
+ 
+});
