c07aa7fd5c7d7a249e01d7f6e7bcd74b802166c3
WillVill <bech.william2@gmail.com>
2015-11-27 00:08:12 +0100

    Committed persistence for test
     I did it on purpose this time!!!

--- test/ApiTest.java
+++ test/ApiTest.java
@@ -1,11 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+
+import com.jayway.restassured.RestAssured;
+import static com.jayway.restassured.RestAssured.*;
+import com.jayway.restassured.parsing.Parser;
+import static javax.swing.UIManager.get;
+import javax.ws.rs.core.MediaType;
+import static org.hamcrest.Matchers.equalTo;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.BeforeClass;
 
 /**
  *
@@ -13,7 +16,41 @@ import static org.junit.Assert.*;
  */
 public class ApiTest {
     
-    public ApiTest() {
+    @BeforeClass
+    public static void setUpBeforeClass(){
+        baseURI = "http://localhost:8080/semesterSeedSP";
+        defaultParser = Parser.JSON;
+        basePath = "/api";
     }
+    
+    @Test
+    public void getUsersTest(){
+        final String uri ="demoadmin/users";
+               given().
+                contentType(MediaType.APPLICATION_JSON).
+                when().
+                get("/demoadmin/users").
+                then().
+                    statusCode(401)
+                ;
+    }
+    
+      @Test
+    public void getUsersLoggedInTest(){
+        final String uri ="demoadmin/users";
+               given().
+                contentType(MediaType.APPLICATION_JSON).
+                when().
+                get("/demoadmin/users").
+                then().
+                    statusCode(200)
+                ;
+    }
+    
+//    @Test
+//    public void loginTest(){
+//        final String uri ="http://localhost:8080/semesterSeedSP/api/demoadmin/users";
+//        final requestSepcification = RestAssured.get(uri).
+//    }
     
 }
